412eccb7e5a546401b4e0cc9712609a812fddf58
badaitofan <badai.javanese@gmail.com>
2025-03-25 11:48:44 +0700

    Alvin : Menambahkan Koneksi Database IMS-ICT

--- application/config/database.php
+++ application/config/database.php
@@ -73,6 +73,29 @@ $db['asset_ict'] = array(
 	'save_queries' => TRUE
 );
 
+// Connect to Database IMS ICT
+$db['ims_ict'] = array(
+	'dsn'	=> '',
+	'hostname' => 'localhost',
+	'username' => 'root',
+	'password' => 'Indonesia8154!!',
+	'database' => 'gudangict',
+	'dbdriver' => 'mysqli',
+	'dbprefix' => '',
+	'pconnect' => FALSE,
+	'db_debug' => (ENVIRONMENT !== 'production'),
+	'cache_on' => FALSE,
+	'cachedir' => '',
+	'char_set' => 'utf8',
+	'dbcollat' => 'utf8_general_ci',
+	'swap_pre' => '',
+	'encrypt' => FALSE,
+	'compress' => FALSE,
+	'stricton' => FALSE,
+	'failover' => array(),
+	'save_queries' => TRUE
+);
+
 // Connect to Database Assets GA
 $db['asset_ga'] = array(
 	'dsn'	=> '',

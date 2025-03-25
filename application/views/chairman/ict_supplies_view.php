f766df5d49b471d2134cdc2a69180f82397c889f
badaitofan <badai.javanese@gmail.com>
2025-03-25 11:56:28 +0700

    Alvin : Modifikasi ict supplies view - membuat tabel data ict supplies

---
+++ application/views/chairman/ict_supplies_view.php
@@ -0,0 +1,77 @@
+<div class="container-fluid">
+    <div class="page-title">
+        <div class="row">
+            <div class="col-xl-4 col-sm-7 box-col-3">
+                <h3>ICT - Supplies</h3>
+            </div>
+            <div class="col-4 d-none d-xl-block">
+                
+            </div>
+            <div class="col-xl-4 col-sm-5 box-col-4">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="<?= site_url('Dashboard')?>">
+                        <svg class="stroke-icon">
+                            <use href="<?=base_url()?>assets/svg/icon-sprite.svg#stroke-home"></use>
+                        </svg></a></li>
+                    <li class="breadcrumb-item">Dir. Chairman</li>
+                    <li class="breadcrumb-item active">ICT</li>
+                    <!-- <li class="breadcrumb-item ">General</li> -->
+                </ol>
+            </div>
+        </div>
+    </div>
+    </div>
+    <!-- Container-fluid starts-->
+    <div class="container-fluid default-dashboard2">
+  <div class="row">              
+    <!-- Complex headers (rowspan and colspan) Starts-->
+    <div class="col-sm-12">
+      <div class="card">
+        <div class="card-header pb-0 card-no-border">
+          <div class="header-top">
+            <h4>Daftar Supplies ICT</h4>
+            <div class="dropdown icon-dropdown setting-menu">
+              <button class="btn dropdown-toggle" id="userdropdown60" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                <svg>
+                  <use href="../assets/svg/icon-sprite.svg#setting"> </use>
+                </svg>
+              </button>
+              <div class="dropdown-menu dropdown-menu-end" aria-labelledby="userdropdown60"><a class="dropdown-item" href="#">Weekly </a><a class="dropdown-item" href="#">Monthly</a><a class="dropdown-item" href="#">Yearly</a></div>
+            </div>
+          </div>
+        </div>
+        <div class="card-body">
+          <div class="table-responsive"> 
+            <table class="display" id="basic-6">
+              <thead>
+                <tr>
+                  <th>No</th>
+                  <th>ID Barang</th>
+                  <th>Nama Barang </th>
+                  <th>Jenis Barang</th>
+                  <th>Stok Saat Ini</th> 
+                </tr>
+              </thead>
+              <tbody>
+                <?php $no=1; foreach ($row as $dataAsset) 
+                  
+                {?>
+                  <tr> 
+                    <td><?= $no++ ?></td>
+                    <td> <?= $dataAsset['id_barang']; ?> </td>
+                    <td> <?= $dataAsset['nama_barang']; ?> </td>
+                    <td> <?= $dataAsset['nama_jenis']; ?> </td>
+                    <td> <?= $dataAsset['stok']; ?> </td>           
+                  </tr>
+             
+                <?php } ?>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Complex headers (rowspan and colspan) Ends-->
+  </div>
+</div>
+<!-- Container-fluid Ends-->

5db5ddd6613f0e7ad86bdc8c60725f1dffbccb3e
badaitofan <badai.javanese@gmail.com>
2025-06-11 15:30:42 +0700

    Alvin : menambahkan link menu IMS

---
+++ application/views/ims/ims-bmg.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>IMS BMG</title>
+  <style>
+    html, body {
+      margin: 0;
+      padding: 0;
+      height: 100%;
+    }
+    iframe {
+      width: 100%;
+      height: 100vh;
+      border: none;
+    }
+  </style>
+</head>
+<body>
+
+  <iframe src="http://10.10.10.23/IMS-BMG/" ></iframe>
+
+</body>
+</html>
